8154c1f40d449bfc76d1a29cf479b1a202950733
서동재 <99057432+arsenalis4@users.noreply.github.com>
2022-05-24 18:52:36 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){ 
+    FadeAnimation();
+})
+
+async function FadeAnimation() {
+    await fade_slider('#eclipse_1, #main_photo_animation_one');
+    await fade_slider('#eclipse_2, #main_photo_animation_two');
+    await fade_slider('#eclipse_3, #main_photo_animation_three');
+    await fade_slider('#eclipse_4, #main_photo_animation_four');
+    FadeAnimation();
+}
+
+function fade_slider(a) {
+  return new Promise((resolve, reject) => {
+      $(a).fadeIn(2000, () => {
+          $(a).fadeOut(2000, () => {
+              resolve();
+          });
+      });
+  })
+}
+
+function display_none(a){
+    $(a).css('display', 'none')
+}
